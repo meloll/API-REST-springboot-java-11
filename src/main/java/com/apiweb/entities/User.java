0dd98abc0262bd189cb5fd11c38f03dcd32d15fb
Guilherme Melo <gui.melobarros10@hotmail.com>
2021-10-21 16:39:32 -0300

    H2 DB e JPA

--- src/main/java/com/apiweb/entities/User.java
+++ src/main/java/com/apiweb/entities/User.java
@@ -3,18 +3,26 @@ package com.apiweb.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 
-
+@Entity
 public class User implements Serializable{
 	
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	
 	private String name;
 	private String email;
 	private String phone;
