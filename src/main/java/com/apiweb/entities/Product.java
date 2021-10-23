f22e06ecee5a1e15681a528bc84ea0460b607d7f
Guilherme Melo <gui.melobarros10@hotmail.com>
2021-10-23 16:38:28 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/apiweb/entities/Product.java
+++ src/main/java/com/apiweb/entities/Product.java
@@ -9,8 +9,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -18,26 +20,26 @@ import lombok.Setter;
 @Setter
 @Getter
 @Entity
-@Table(name="tb_product")
-public class Product implements Serializable  {
+@Table(name = "tb_product")
+public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	private  String description;
+	private String description;
 	private double price;
-	private  String imgUrl;
-	
-	@Transient
-	private Set<Category> categories= new HashSet<>() ;
-	
-	
-	public Product() {
-		
-	}
+	private String imgUrl;
 
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
+
+	public Product() {
+
+	}
 
 	public Product(Long id, String name, String description, double price, String imgUrl) {
 		super();
@@ -48,12 +50,10 @@ public class Product implements Serializable  {
 		this.imgUrl = imgUrl;
 	}
 
-
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -66,7 +66,5 @@ public class Product implements Serializable  {
 		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	};
-	
-	
 
 }
