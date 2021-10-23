5d83bc66862287ecb3c84c4ee6e15bf19878dbd8
Guilherme Melo <gui.melobarros10@hotmail.com>
2021-10-23 14:48:59 -0300

    Category

--- src/main/java/com/apiweb/config/TestConfig.java
+++ src/main/java/com/apiweb/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.apiweb.entities.Category;
 import com.apiweb.entities.Order;
 import com.apiweb.entities.User;
 import com.apiweb.entities.enums.OrderStatus;
+import com.apiweb.repositories.CategoryRepository;
 import com.apiweb.repositories.OrderRepository;
 import com.apiweb.repositories.UserRepository;
 
@@ -23,9 +25,19 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		
+		
 		// TODO Auto-generated method stub
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
@@ -36,6 +48,8 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2,OrderStatus.WAITING_PAYMENT);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1,OrderStatus.WAITING_PAYMENT);
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		
 		
 	}
 	
---
+++ src/main/java/com/apiweb/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.apiweb.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.apiweb.entities.Category;
+
+
+
+public interface CategoryRepository<CategoryRepository> extends JpaRepository<Category, Long > {
+
+}
