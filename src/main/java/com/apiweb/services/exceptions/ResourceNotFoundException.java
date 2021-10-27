4770685d5661d1d65f16471c8c009289bfe697fe
Guilherme Melo <gui.melobarros10@hotmail.com>
2021-10-27 18:13:20 -0300

    exception handling

---
+++ src/main/java/com/apiweb/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package com.apiweb.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id "+id);
+	}
+	
+	
+}
