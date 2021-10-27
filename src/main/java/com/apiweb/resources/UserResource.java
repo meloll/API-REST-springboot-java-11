f291b7e74bdb3758badfaa25f976f3f6ff064539
Guilherme Melo <gui.melobarros10@hotmail.com>
2021-10-27 17:35:43 -0300

    delete

--- src/main/java/com/apiweb/resources/UserResource.java
+++ src/main/java/com/apiweb/resources/UserResource.java
@@ -9,6 +9,7 @@ import javax.servlet.Servlet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,5 +53,10 @@ public class UserResource {
 		return ResponseEntity.created(uri).body(obj);
 	}
 	
+	@DeleteMapping(value ="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 
 }
